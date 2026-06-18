83540e98673af1166d8b8e13468937dbb2df13e6
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-06-18 15:29:09 +0500

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,5 @@
+#bu darsda biz asosan visual studio qanday ishlashi fayllar bilan ishlash, va umumiy olganda code yozish uchun hamma kerakli narsalarni sozlab oldik
+print(" python file yaratganimizda fayl nomidan keyin (.py) qoshimchasini qoshishimiz kerak")
+#birinchi dasturimiz 
+print("Hello World!")
+#siz endi dasturchisiz! ha ha ha.
