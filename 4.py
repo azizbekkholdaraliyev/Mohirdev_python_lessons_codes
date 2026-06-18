c66dcd5db5b3c6502c4894e292ad05c42342f52c
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-06-18 15:54:56 +0500

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,19 @@
+#ozgaruvchilar malum bir malumotni saqlash uchun komputer hotirasidan ajratilgan joy desak boladi
+print("ozgaruvchilarni quti deb olsak boladi")
+
+ism = "Anvar"
+yosh = 18
+print(yosh)
+
+ism = "Sarvar"
+print(ism)
+ism = "Olim"
+
+a = 7
+b = 8
+c = a * b
+print(c)
+print(a + b)
+
+print("dasturlashda katta va kichik harflar alohida talqin qilinadi shuning uchun iloji boricha kichkina harfalardan bolgan ozgaruchi yaratishga harakat qil")
+
