57c93afc08d6dc99aa4f0d3d9c8bc6ed8f141e03
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-06-18 15:54:53 +0500

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,21 @@
+#print funksiyasi va hashtag(#) va arifmetik amallarni bajarishni korib chiqdik
+#syntaxError haqida gaplashdik
+
+#hashtag
+
+print("hayrli tong!")
+
+print("console ni tozalash uchun clear yoki ctrl + l dan foydalan ")
+
+print("matnni qatorlarga bolish uchun ''' yoki \n '\ n' dan foydalan ")
+
+print("""hello world,
+hello world""")
+
+print("arifmetik amallar + - / * ")
+
+print(4 + 5 + 9 + 2) #qoshish
+print(4 / 2) #bolish
+print(2**3) #darajaga oshirish
+print(4 - 1) #ayirish
+print(4 * 2) #kopaytirish
