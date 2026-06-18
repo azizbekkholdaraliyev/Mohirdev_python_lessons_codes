e538baccca4009c59701cf2fa8d9224705ffd98e
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-06-18 15:14:58 +0500

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,10 @@
+#bu darsda men kerakli dasturlarni ornatishni organdim bizga kerak bolarkan: visual studi, python va shunga oxshash kerakli ilovalar
+print("Visual Studio and Python instalition")
+
+
+
+
+
+
+
+
