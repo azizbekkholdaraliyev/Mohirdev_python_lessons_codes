36064c7ffe037ec8976fa8f39940004f5e2ed6c6
azizbekkholdaraliyev <azizbekholdaraliyev@gmail.com>
2026-07-01 22:09:45 +0500

    Create 13.py

---
+++ 13.py
@@ -0,0 +1,3 @@
+#bu darsda GitHub platformasi bilan qanday ishlash unga codelarni yuklash, boshqalarni codelarini yuklab olish va vahokazolarni korib chiqdik 
+#men bularni oldindan bilardim shuning uchun darsi bir marta boshidan oxirigacha toliq kordim yani qaytarib olish uchun va ularga kop toxtalmadm
+#keyingi dasrga otsak boladi
